81449c658f697aeffb2453072f54e2cfa2d74bfc
hatoya <121746047+hato-ya@users.noreply.github.com>
2024-07-28 15:25:46 +0900

    Update targetSdkVersion to 34 and migrate to Android Studio Koala and Gradle 8.5.1

--- app/src/androidTest/java/com/hatopigeon/cubicify/ApplicationTest.java
+++ app/src/androidTest/java/com/hatopigeon/cubicify/ApplicationTest.java
@@ -1,13 +1,22 @@
 package com.hatopigeon.cubicify;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = ApplicationProvider.getApplicationContext();
+        assertEquals("com.hatopigeon.cubicify", appContext.getPackageName());
     }
 }
